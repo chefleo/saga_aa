3104d49044828f4b0aff9f68645d918555798dc0
chefleo <simon.digiorgio@gmail.com>
2024-02-13 16:24:43 +0100

    Clean up script execute.js

--- scripts/localhost/execute.js
+++ scripts/localhost/execute.js
@@ -2,25 +2,24 @@ const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 const FACTORY_NONCE = 1;
-const FACTORY_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const EP_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-const SimpleStorage_ADDR = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
+const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const EP_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const SimpleStorage_ADDR = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 async function main() {
   const entryPoint = await hre.ethers.getContractAt("EntryPoint", EP_ADDRESS);
+  const AccountFactory = await hre.ethers.getContractFactory("AccountFactory");
+  const simpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
+  const Account = await hre.ethers.getContractFactory("Account");
+
+  // This will be the owner of the smart account
+  const [signer0] = await hre.ethers.getSigners();
+  const address0 = await signer0.getAddress();
 
   const sender = await hre.ethers.getCreateAddress({
     from: FACTORY_ADDRESS,
     nonce: FACTORY_NONCE,
   });
-
-  console.log("Sender:", { sender });
-
-  const AccountFactory = await hre.ethers.getContractFactory("AccountFactory");
-
-  // This will be the owner of the smart account
-  const [signer0, signer1] = await hre.ethers.getSigners();
-  const address0 = await signer0.getAddress();
 
   const nonce = await entryPoint.getNonce(sender, 0);
 
@@ -34,6 +33,12 @@ async function main() {
         .encodeFunctionData("createAccount", [address0])
         .slice(2);
   }
+
+  // Encoded function needed for calldata in userOp
+  const simpleStorageEncoded = simpleStorage.interface.encodeFunctionData(
+    "set",
+    [15]
+  );
 
   const balance = await entryPoint.balanceOf(sender);
   const balanceEth = hre.ethers.formatUnits(balance, "ether");
@@ -50,20 +55,9 @@ async function main() {
     console.log("Smart Wallet funded");
   }
 
-  // AddressBook Contract and encoded function
-  const simpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
-
-  // Encoded function needed for calldata in userOp
-  const simpleStorageEncoded = simpleStorage.interface.encodeFunctionData(
-    "set",
-    [10]
-  );
-
-  const Account = await hre.ethers.getContractFactory("Account");
-
   const userOp = {
     sender, // smart account address
-    nonce: await entryPoint.getNonce(sender, 0),
+    nonce,
     initCode: initCode, // Creation of the wallet
     callData: Account.interface.encodeFunctionData("execute", [
       SimpleStorage_ADDR,
