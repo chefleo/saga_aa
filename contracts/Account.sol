1ecf9c2fbd9e1f9dbde1aee9d907d95c309a62ee
chefleo <simon.digiorgio@gmail.com>
2024-02-13 16:22:37 +0100

    Small clean in Account.sol

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -23,13 +23,13 @@ contract Account is IAccount {
   }
 
   function _call(address target, uint256 value, bytes memory data) internal {
-        (bool success, bytes memory result) = target.call{value: value}(data);
-        if (!success) {
-            assembly {
-                revert(add(result, 32), mload(result))
-            }
+    (bool success, bytes memory result) = target.call{value: value}(data);
+    if (!success) {
+        assembly {
+            revert(add(result, 32), mload(result))
         }
     }
+  }
 }
 
 contract AccountFactory {
